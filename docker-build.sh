e193ee54e3870ff0ce56ae20384fe08fbc3f5ec0
Zexi Li <zexi.li@icloud.com>
2025-04-10 19:36:48 +0800

    update docker-build.sh

--- docker-build.sh
+++ docker-build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-TAG="ports.8-pulse.1"
+TAG="${TAG:-hook.1}"
 IMG="registry.cn-beijing.aliyuncs.com/zexi/wolf:$TAG"
 
   # --build-arg HTTPS_PROXY="http://192.168.167.128:7890" --build-arg HTTP_PROXY="http://192.168.167.128:7890" \
-docker build \
-  --build-arg HTTPS_PROXY="http://192.168.6.60:7890" --build-arg HTTP_PROXY="http://192.168.6.60:7890" \
+  # --build-arg HTTPS_PROXY="http://192.168.6.60:7890" --build-arg HTTP_PROXY="http://192.168.6.60:7890" \
+docker buildx build --platform linux/amd64 --push \
   -t "$IMG" \
   -f ./docker/wolf.Dockerfile .
 
-docker push $IMG
+#docker push $IMG
