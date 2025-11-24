d582c080cd6657e784dab3d26f00bfeca7d4d383
Zexi Li <zexi.li@icloud.com>
2025-11-24 21:49:41 +0800

    update the build script

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-TAG := static-20251015.0
+TAG := static-20251124.0
 
 .PHONY: build
 
--- docker-build.sh
+++ docker-build.sh
@@ -3,10 +3,10 @@
 TAG="${TAG:-hook.1}"
 IMG="registry.cn-beijing.aliyuncs.com/zexi/wolf:$TAG"
 
-PROXY_SERVER=192.168.6.60
+#PROXY_SERVER=192.168.6.60
+  #--build-arg HTTPS_PROXY="http://$PROXY_SERVER:7890" --build-arg HTTP_PROXY="http://$PROXY_SERVER:7890" --build-arg ALL_PROXY="socks5://$PROXY_SERVER:7890" \
 
 docker buildx build --platform linux/amd64 --push \
-  --build-arg HTTPS_PROXY="http://$PROXY_SERVER:7890" --build-arg HTTP_PROXY="http://$PROXY_SERVER:7890" --build-arg ALL_PROXY="socks5://$PROXY_SERVER:7890" \
   -t "$IMG" \
   -f ./docker/wolf.Dockerfile .
 
